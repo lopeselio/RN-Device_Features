8a26a18219a96316c82ebebded5c2380af3b4c55
lopeselio <lopeselio@gmail.com>
2020-10-30 23:27:49 +0530

    added RSC to screens

---
+++ screens/PlaceDetailScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const PlaceDetailsScreen = props => {
+  return <View>
+    <Text>PlacesListScreen</Text>
+  </View>
+}
+
+const styles = StyleSheet.create({})
+
+export default PlaceDetailsScreen 

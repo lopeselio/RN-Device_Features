ad1d183e42303d9af64096b77efd7c10110bc39a
lopeselio <lopeselio@gmail.com>
2020-10-30 23:20:58 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import PlacesNavigator from './navigation/PlacesNavigator'
+export default function App() {
+  return (
+    return <PlacesNavigator />
+  );
+}
+

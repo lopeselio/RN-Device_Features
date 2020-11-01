762d8a5008bdda2b32d5d3d0c311ad5b42135dc8
lopeselio <lopeselio@gmail.com>
2020-11-01 15:16:18 +0530

    added place model

--- models/place.js
+++ models/place.js
@@ -1,8 +1,8 @@
 class Place {
   constructor(id, title) {
-    this.id = id
-    this.title = title
+      this.id = id;
+      this.title = title;
   }
-} 
+}
 
-export default Place
+export default Place;

68481f1162a591e64892c346b40c98198ac41035
lopeselio <lopeselio@gmail.com>
2020-11-01 15:16:34 +0530

    added actions and reducers for images

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,5 +1,5 @@
-export const ADD_PLACE = 'ADD_PLACE'
+export const ADD_PLACE = 'ADD_PLACE';
 
 export const addPlace = title => {
-  return { type: ADD_PLACE, placeData: { title: title } }
-}
+  return { type: ADD_PLACE, placeData: { title: title } };
+};
--- store/places-reducers.js
+++ store/places-reducers.js
@@ -1,18 +1,18 @@
-import { ADD_PLACE } from './places-actions'
-import Place from '../models/place'
+import { ADD_PLACE } from './places-actions';
+import Place from '../models/place';
 
 const initialState = {
   places: []
-}
+};
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case ADD_PLACE:
-      const newPlace = new Place(new Date().toString(), action.placeData.Title)
+      const newPlace = new Place(new Date().toString(), action.placeData.title);
       return {
         places: state.places.concat(newPlace)
-      }
+      };
     default:
-      return state
+      return state;
   }
-}
+};

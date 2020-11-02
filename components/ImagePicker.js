434b81f22e4fe024178cb7a6115a95ff6273e4ac
lopeselio <lopeselio@gmail.com>
2020-11-02 10:10:17 +0530

    configured camera access

--- components/ImagePicker.js
+++ components/ImagePicker.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Button, Image, Text, StyleSheet, Alert } from 'react-native'
 import * as ImagePicker from 'expo-image-picker'
 import * as Permissions from 'expo-permissions'
@@ -6,6 +6,7 @@ import * as Permissions from 'expo-permissions'
 import Colors from '../constants/Colors'
 
 const ImgPicker = props => {
+  const [pickedImage, setPickedImage] = useState()
   const verifyPermissions = async () => {
     const result = await Permissions.askAsync(Permissions.CAMERA_ROLL)
     if (result.status !== 'granted') {
@@ -24,14 +25,26 @@ const ImgPicker = props => {
     if (!hasPermission) {
       return
     }
-    ImagePicker.launchCameraAsync()
+    const image = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5
+    })
+
+    // console.log(image)
+    setPickedImage(image.uri)
   }
 
   return (
     <View style={styles.imagePicker}>
       <View style={styles.imagePreview}>
-        <Text>No image picked yet.</Text>
-        <Image style={styles.image} />
+        {
+          !pickedImage ? (
+            <Text>No image picked yet.</Text>
+          ) : (
+            <Image style={styles.image} source={{ uri: pickedImage }} />
+          )
+        }
       </View>
       <Button
         title='Take Image'

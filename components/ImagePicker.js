cd4f20a69711d42d2632d6ba9039c44ef46480ff
lopeselio <lopeselio@gmail.com>
2020-11-02 10:56:59 +0530

    added func to use the picked image

--- components/ImagePicker.js
+++ components/ImagePicker.js
@@ -33,6 +33,7 @@ const ImgPicker = props => {
 
     // console.log(image)
     setPickedImage(image.uri)
+    props.onImageTaken(image.uri)
   }
 
   return (
@@ -57,7 +58,8 @@ const ImgPicker = props => {
 
 const styles = StyleSheet.create({
   imagePicker: {
-    alignItems: 'center'
+    alignItems: 'center',
+    marginBottom: 15
   },
   imagePreview: {
     width: '100%',

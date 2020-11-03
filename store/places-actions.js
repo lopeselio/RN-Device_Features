0d937ca484186e275babc9c1ccfe8a495f88e260
lopeselio <lopeselio@gmail.com>
2020-11-03 20:38:38 +0530

    file system

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,5 +1,10 @@
-export const ADD_PLACE = 'ADD_PLACE';
+import * as FileSystem from 'expo-file-system'
+
+export const ADD_PLACE = 'ADD_PLACE'
 
 export const addPlace = (title, image) => {
-  return { type: ADD_PLACE, placeData: { title: title, image: image } };
-};
+  return async dispatch => {
+    const newPath = FileSystem.documentDirectory
+    dispatch({ type: ADD_PLACE, placeData: { title: title, image: image } })
+  }
+}

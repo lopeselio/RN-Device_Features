c6a87dae7813877be2c7e04c8d21c280a759a1bb
lopeselio <lopeselio@gmail.com>
2020-11-08 16:41:46 +0530

    navigator

--- navigation/PlacesNavigator.js
+++ navigation/PlacesNavigator.js
@@ -1,11 +1,11 @@
-import { Platform } from 'react-native';
-import { createStackNavigator } from "react-navigation-stack";
-import { createAppContainer } from "react-navigation";
-import PlacesListScreen from '../screens/PlacesListScreen';
-import PlaceDetailScreen from '../screens/PlaceDetailScreen';
-import NewPlaceScreen from '../screens/NewPlaceScreen';
-import MapScreen from '../screens/MapScreen';
-import Colors from '../constants/Colors';
+import { Platform } from 'react-native'
+import { createStackNavigator } from 'react-navigation-stack'
+import { createAppContainer } from 'react-navigation'
+import PlacesListScreen from '../screens/PlacesListScreen'
+import PlaceDetailScreen from '../screens/PlaceDetailScreen'
+import NewPlaceScreen from '../screens/NewPlaceScreen'
+import MapScreen from '../screens/MapScreen'
+import Colors from '../constants/Colors'
 
 const PlacesNavigator = createStackNavigator(
   {
@@ -22,6 +22,6 @@ const PlacesNavigator = createStackNavigator(
       headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
     }
   }
-);
+)
 
-export default createAppContainer(PlacesNavigator);
+export default createAppContainer(PlacesNavigator)

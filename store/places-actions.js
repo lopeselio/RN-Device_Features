5ab0c0e3e48db41bf6f0731930123ec4ea35a06d
lopeselio <lopeselio@gmail.com>
2020-11-08 11:22:36 +0530

    export action places SET_PLACES

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,7 +1,8 @@
 import * as FileSystem from 'expo-file-system'
-import { insertPlace } from '../helpers/db'
+import { insertPlace, fetchPlaces } from '../helpers/db'
 
 export const ADD_PLACE = 'ADD_PLACE'
+export const SET_PLACES = 'SET_PLACES'
 
 export const addPlace = (title, image) => {
   return async dispatch => {
@@ -24,6 +25,18 @@ export const addPlace = (title, image) => {
       dispatch({ type: ADD_PLACE, placeData: { id: dbResult.insertId, title: title, image: newPath } })
     } catch (err) {
       console.log(err)
+      throw err
+    }
+  }
+}
+
+export const loadPlaces = () => {
+  return async dispatch => {
+    try {
+      const dbResult = await fetchPlaces()
+      console.log(dbResult)
+      dispatch({ type: SET_PLACES, places: [] })
+    } catch (err) {
       throw err
     }
   }

4570b1a29e0a8e8f14c6088bce4bc1cce67b3448
lopeselio <lopeselio@gmail.com>
2020-11-08 12:14:15 +0530

    List

--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -1,19 +1,20 @@
-import React, { useEffect } from 'react';
-import { View, Text, StyleSheet, Platform, FlatList } from 'react-native';
-import { HeaderButtons, Item } from 'react-navigation-header-buttons';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useEffect } from 'react'
+import { View, Text, StyleSheet, Platform, FlatList } from 'react-native'
+import { HeaderButtons, Item } from 'react-navigation-header-buttons'
+import { useSelector, useDispatch } from 'react-redux'
 
-import HeaderButton from '../components/HeaderButton';
-import PlaceItem from '../components/PlaceItem';
+import HeaderButton from '../components/HeaderButton'
+import PlaceItem from '../components/PlaceItem'
 import * as placesActions from '../store/places-actions'
 
 const PlacesListScreen = props => {
-  const places = useSelector(state => state.places.places);
+  const places = useSelector(state => state.places.places)
   const dispatch = useDispatch()
 
   useEffect(() => {
-    dispatch(placesActions.loadPlaces)
+    dispatch(placesActions.loadPlaces())
   }, [dispatch])
+
   return (
     <FlatList
       data={places}
@@ -32,7 +33,7 @@ const PlacesListScreen = props => {
         />
       )}
     />
-  );
+  )
 };
 
 PlacesListScreen.navigationOptions = navData => {
@@ -41,17 +42,17 @@ PlacesListScreen.navigationOptions = navData => {
     headerRight: (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
         <Item
-          title="Add Place"
+          title='Add Place'
           iconName={Platform.OS === 'android' ? 'md-add' : 'ios-add'}
           onPress={() => {
-            navData.navigation.navigate('NewPlace');
+            navData.navigation.navigate('NewPlace')
           }}
         />
       </HeaderButtons>
     )
-  };
+  }
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({})
 
-export default PlacesListScreen;
+export default PlacesListScreen

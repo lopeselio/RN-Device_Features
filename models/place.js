b93ce718301aca3d7b65260f5bfe7c77be8c4056
lopeselio <lopeselio@gmail.com>
2020-11-09 16:15:01 +0530

    place

--- models/place.js
+++ models/place.js
@@ -1,9 +1,12 @@
 class Place {
-  constructor (id, title, imageUri) {
-    this.id = id
-    this.title = title
-    this.imageUri = imageUri
+  constructor(id, title, imageUri, address, lat, lng) {
+    this.id = id;
+    this.title = title;
+    this.imageUri = imageUri;
+    this.address = address;
+    this.lat = lat;
+    this.lng = lng;
   }
 }
 
-export default Place
+export default Place;
